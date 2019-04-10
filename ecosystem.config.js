4f4093a4effe51e2eb2a70a32c1b59bbdba97df2
JC-OH <jicheol@gmail.com>
2019-04-10 12:15:17 -0700

    35 pm2로 실제 서버에 배포하기

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps : [{
+    name      : 'nemv',
+    script    : './be/bin/www',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 3000
+    },
+    env_pr : {
+      NODE_ENV: 'production',
+      PORT: 8020
+    }
+  }],
+
+  // deploy : {
+  //   production : {
+  //     user : 'node',
+  //     host : '212.83.163.1',
+  //     ref  : 'origin/master',
+  //     repo : 'git@github.com:repo.git',
+  //     path : '/var/www/production',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  //   }
+  // }
+};
