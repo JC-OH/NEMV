5e8f5bb2da9409d78373c45bce85e42a84f52acf
JC-OH <jicheol@gmail.com>
2019-04-11 11:54:44 -0700

    토큰 검사하는 미들웨어는 토큰이 있을때만 들어와야되기 때문에 sign 밑에 있는 것입니다.

--- be/routes/api/index.js
+++ be/routes/api/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router()
 const createError = require('http-errors')
 
+router.use('/sign', require('./sign'));
+router.all('*', function(req, res, next) {
+  // 토큰 검사
+  next()
+});
 router.use('/user', require('./user'));
 router.use('/crud', require('./crud'));
-router.use('/sign', require('./sign'));
+
 
 router.all('*', require('./notFound'));
 
