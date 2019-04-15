488765de0deedfaa5bea1c4e4626e80506151a9b
JC-OH <jicheol@gmail.com>
2019-04-15 12:31:35 -0700

    56 회원가입 프론트 적용하기

--- fe/src/views/Register.vue
+++ fe/src/views/Register.vue
@@ -1,99 +1,158 @@
 <template>
 
   <v-container fluid fill-height>
-    <v-layout align-center justify-center>
-   <v-flex xs12 sm8 md4>
-     <v-card class="elevation-12">
-       <v-toolbar dark color="primary">
-         <v-toolbar-title>회원 가입</v-toolbar-title>
-         <v-spacer></v-spacer>
-       </v-toolbar>
-       <v-card-text>
-         <form>
-           <v-text-field
-             v-validate="'required|max:10'"
-             v-model="name"
-             :counter="10"
-             :error-messages="errors.collect('name')"
-             label="Name"
-             data-vv-name="name"
-             required
-           ></v-text-field>
-           <v-text-field
-             v-validate="'required|email'"
-             v-model="email"
-             :error-messages="errors.collect('email')"
-             label="E-mail"
-             data-vv-name="email"
-             required
-           ></v-text-field>
-           <v-select
-             v-validate="'required'"
-             :items="items"
-             v-model="select"
-             :error-messages="errors.collect('select')"
-             label="Select"
-             data-vv-name="select"
-             required
-           ></v-select>
-           <v-checkbox
-             v-validate="'required'"
-             v-model="checkbox"
-             :error-messages="errors.collect('checkbox')"
-             value="1"
-             label="Option"
-             data-vv-name="checkbox"
-             type="checkbox"
-             required
-           ></v-checkbox>
 
-           <v-btn @click="submit">submit</v-btn>
-           <v-btn @click="clear">clear</v-btn>
-         </form>
-       </v-card-text>
-     </v-card>
-   </v-flex>
- </v-layout>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>회원 가입</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <form>
+                    <!-- v-model로 값들과 양방향 바인딩 상태입니다. -->
+                    <!-- :counter의 경우 그저 표시용입니다. 제한사항은 아님! -->
+                  <v-text-field
+                    v-validate="'required|min:4|max:20'"
+                    v-model="form.id"
+                    :counter="20"
+                    :error-messages="errors.collect('id')"
+                    label="아이디"
+                    data-vv-name="id"
+                    required
+                  ></v-text-field>
+                  <!-- 텍스트 필드 룰: required|min:4|max:20 이면 꼭 넣어야하고 4글자 이상 20글자 이하 인것이죠 -->
+                  <v-text-field
+                    v-validate="'required|min:6|max:40'"
+                    v-model="form.pwd"
+                    :counter="40"
+                    :error-messages="errors.collect('pwd')"
+                    label="비밀번호"
+                    data-vv-name="pwd"
+                    required
+                    type="password"
+                  ></v-text-field>
+                  <!-- error.collect()로 아마도 data-vv-name와 연결되어 메세지가 표시되는 것 같습니다.(자세히는 모름..) -->
+                  <v-text-field
+                    v-validate="'required|min:1|max:40'"
+                    v-model="form.name"
+                    :counter="40"
+                    :error-messages="errors.collect('name')"
+                    label="이름"
+                    data-vv-name="name"
+                    required
+                  ></v-text-field>
+
+                  <v-checkbox
+                    v-validate="'required'"
+                    v-model="agree"
+                    :error-messages="errors.collect('agree')"
+                    value="1"
+                    label="약관동의: 암호화도 안되어 있는 사이트인데 정말 가입하겠습니까?"
+                    data-vv-name="agree"
+                    type="checkbox"
+                    required
+                  ></v-checkbox>
+
+                  <v-btn @click="submit">가입</v-btn>
+                  <v-btn @click="clear">초기화</v-btn>
+                </form>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+        <v-snackbar
+          v-model="sb.act"
+        >
+          {{ sb.msg }}
+          <v-btn
+            :color="sb.color"
+            flat
+            @click="sb.act = false"
+          >
+            닫기
+          </v-btn>
+        </v-snackbar>
   </v-container>
 </template>
 <script>
+import ko from 'vee-validate/dist/locale/ko'
+
 export default {
   $_veeValidate: {
-    validator: 'new'
-  },
+  validator: 'new'
+},
 
-  data() {
-    return {
-      name: '',
-      email: '',
-      select: null,
-      items: [
-         'Item 1',
-         'Item 2',
-         'Item 3',
-         'Item 4'
-      ],
-      checkbox: null,
-      dictionary: {
-       attributes: {
-         email: 'E-mail Address'
-         // custom attributes
-       },
-       custom: {
-         name: {
-           required: () => 'Name can not be empty',
-           max: 'The name field may not be greater than 10 characters'
-           // custom messages
-         },
-         select: {
-           required: 'Select field is required'
-         }
-       }
-     }
-    }
+data: () => ({
+  // 전송될 데이터를 form 변수에 담았습니다.
+  form: {
+    id: '',
+    name: '',
+    pwd: ''
   },
-  mounted() {
-    this.$validator.localize('en', this.dictionary);
+  sb: {
+    act: false,
+    msg: '',
+    color: 'warning'
+  },
+  agree: null,
+  // dictionary.attr.. 는 메세지에서 ‘id’를 ‘아이디’로 표시해줍니다.
+  dictionary: {
+    messages: ko.messages,
+    attributes: {
+      id: '아이디',
+      pwd: '비밀번호',
+      name: '이름',
+      agree: '약관동의'
+      // custom attributes
+    },
+    custom: {
+      // name: {
+      //   required: () => 'Name can not be empty',
+      //   max: 'The name field may not be greater than 10 characters'
+      //   // custom messages
+      // },
+      // select: {
+      //   required: 'Select field is required'
+      // }
+    }
   }
+}),
+
+mounted () {
+  this.$validator.localize('ko', this.dictionary)
+},
+
+methods: {
+  submit () {
+    // this.$validator.validateAll() 로 false 가 나올경우 에러로 보냅니다.
+    this.$validator.validateAll()
+      .then(r => {
+        if (!r) throw new Error('모두 기입해주세요')
+        // 액시오스 요청으로 form 내용을 전송합니다.
+        return this.$axios.post('register', this.form)
+      })
+      .then(r => {
+        if (!r.data.success) throw new Error('서버가 거부했습니다.')
+        this.pop('가입 완료 되었습니다.', 'success')
+
+        this.$route.push('/sign')
+      })
+      .catch(e => this.pop(e.message, 'warning'))
+  },
+  pop (m, cl) {
+    this.sb.act = true
+    this.sb.msg = m
+    this.sb.color = cl
+  },
+  clear () {
+    this.form.id = ''
+    this.form.pwd = ''
+    this.form.name = ''
+    this.agree = null
+    this.$validator.reset()
+  }
+}
 }
 </script>
